08e27f4cf67b3507317bc42f661881e615bace35
Allan Kimmer Jensen <allankimmerjensen@gmail.com>
2013-03-20 11:27:44 +0100

    Controller Types

--- static/js/controller.js
+++ static/js/controller.js
@@ -14,23 +14,48 @@ var GC = {
 	}
 }
 
+// Controller Types
+var CT = {
+	buttons: {
+		init: function () {
+
+			$('#up').on('click', function() {
+				GC.sendMovement( { dir: 'n' });
+			});
+
+			$('#down').on('click', function() {
+				GC.sendMovement( { dir: 's' } );
+			});
+
+		},
+		destory: function () {
+
+		}
+	},
+	arrowkeys:  {
+		init: function () {
+
+			$(document).keydown(function(evt){
+			  if (evt.which == 38) { // up
+		        GC.sendMovement({ dir: 'n' });
+
+			  } else if (evt.which == 40) { // down
+		        GC.sendMovement({ dir: 's' });
+			  }
+		      console.log('key pressed: ' + evt.which);
+		    });
+
+		},
+		destory: function () {
+
+		}
+	}
+}
+
 $(function() {
-	$(document).keydown(function(evt){
-	  if (evt.which == 38) { // up
-        GC.sendMovement({ dir: 'n' });
 
-	  } else if (evt.which == 40) { // down
-        GC.sendMovement({ dir: 's' });
-	  }
-      console.log('key pressed: ' + evt.which);
-    });
-
-	$('#up').on('click', function() {
-		GC.sendMovement( { dir: 'n' });
-	});
-
-	$('#down').on('click', function() {
-		GC.sendMovement( { dir: 's' } );
-	});
+	// We need to control active input types by initing them and destorying them after
+	CT.buttons.init();
+	CT.arrowkeys.init();
 
 });
